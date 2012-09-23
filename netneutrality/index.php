f685dd00264a6c568d4a82bc6e25dba420880b8e
Thomas Lohninger <socialhack@shc-web4.netzfreiheit.org>
2012-09-23 23:20:42 +0000

    css upgrade
     english definition of net neutrality

---
+++ netneutrality/index.php
@@ -0,0 +1,61 @@
+<?php include($_SERVER['DOCUMENT_ROOT'].'/includes/header.php'); ?>
+
+
+
+		<div id="spoiler" class="inset">
+			<p style="font-weight:bold;">The basic idea of the Internet is a free, uncensored and equitable communication. </p>
+			<p>Net neutrality guarantees freedom of the internet, innovation, freedom and equality.</p>
+		</div>
+
+		<div class="inset">
+		
+			<h3>Net neutrality means:</h3>
+			<ul class="nolist definitionlist">
+				<li id="gleichbehandlung">
+					<img class="definitionpic" src="/images/definition/gleichbehandlung.png">
+					<h4><a name="gleichbehandlung">Equality</a></h4>
+					<p>All data packets have to be treated equaly during transmission with regard to source, destination, class or tariff. Network management can only be justified with technical intentions for preservation of network stability. Interventions in the network are not allowed to depend on business practices or political motivation. </p>
+				</li>
+
+				
+				<li id="teilhabe">
+					<img class="definitionpic" src="/images/definition/teilhabe.png">
+					<h4><a name="teilhabe">Participation</a></h4>
+					<p>An internet connection has to guarantee the full participation on the network. This Participation includes the usage and providing of any service independently of the underlying technology. </p>
+				</li>
+
+
+				<li id="unversehrtheit">
+					<img class="definitionpic" src="/images/definition/unversehrtheit.png">
+					<h4><a name="unversehrtheit">Integrity of data</a></h4>
+					<p>Intervention in the operation method or the content of a data transmission are not allowed. </p>
+				</li>
+
+
+				<li id="zugang">
+					<img class="definitionpic" src="/images/definition/zugang.png">
+					<h4><a name="zugang">Access</a></h4>
+					<p>The Internet Service Provider is not allowed to restrict the access of consumer periphery (Hardware or Software) to it's supplied network connection in any legal or technical way. </p>
+				</li>
+
+
+				<li id="transparenz">
+					<img class="definitionpic" src="/images/definition/transparenz.png">
+					<h4><a name="transparenz">Transparency</a></h4>
+					<p>Internet Service Provider have to state in their contracts and advertisements a guaranteed minimum bandwidth, maximum latency and quality measures for the connection. The Internet Service Provider has to provide tools to verify those standards. These standards have to be determined with an statistical method which has to be published. Network management interventions (see point 1 Equality) have to be justified with the stablity of the network and have to be disclosure to the consumer and the regulation authority. </p>
+				</li>
+
+				<li id="garantie">
+					<img class="definitionpic" src="/images/definition/garantie.png">
+					<h4><a name="garantie">Guarantee</a></h4>
+					<p>The bandwidth, latency and quality measures advertised to the customer and guaranteed in the contract are binding for the internet service provider.</p>
+				</li>
+
+				<li>
+					<h4><a name="durchsetzung">Enforcement</a></h4>
+					<p>Violations of the above conditions can be reported by the end user to an authority defined by the government. This authority must have the rights and resources to enforce above conditions. The government guarantees that the violating party can be held responsible. </p>
+				</li>				
+
+				
+		</div>
+<?php include('../includes/footer.php'); ?>
