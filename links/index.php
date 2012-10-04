f1d5ba782e4ba10fbbae99bebd716981b9494101
socialhack <thomas@lohninger.org>
2012-10-04 16:46:04 +0200

    neuer link

--- links/index.php
+++ links/index.php
@@ -12,25 +12,18 @@
 			
 <ul class="nolist">
 	
-	<li><a href="http://www.netzkinder.at/netzneutralitat-uber-die-unteilbarkeit-des-internets/"><strong>Vortrag: zur Netzneutralit&auml;t und dieser Kampagne</strong></a> von Thomas Lohninger auf der #DNP12 Konferenz</li>
-	
 
+	<li><a href="http://www.netzkinder.at/netzneutralitat-uber-die-unteilbarkeit-des-internets/"><strong>Vortrag: zur Netzneutralit&auml;t und dieser Kampagne</strong></a> von Thomas Lohninger auf der #DNP12 Konferenz</li>
 	<li><a href="http://www.fhstp.ac.at/forschung/institute_bereiche/institutfuermedienwirtschaft/aktuelles/news/12/aktuelle-neuerscheinung-netzneutralitaet-und-netzbewirtschaftung"><strong>Buch: "Netzneutralit&auml;t und Netzbewirtschaftung"</strong></a> von Prof. (FH) Mag. Dr. Tassilo Pellegrini u. Prof. (FH) Dr. Jan Krone</li>
 	<li><a href="https://unsernetz.at/files/Steckbrief_Netzneutralitaet_Pellegrini.pdf"><strong>Steckbrief: Netzneutralit&auml;t</strong></a> von Tassilo Pellegrini</li>
 	<li><a href="https://netarchitecture.org/"><strong>Barbara van Schewick. 2010. Internet Architecture and Innovation. MIT Press. </strong></a></li>
-
 	<li><a href="http://echtesnetz.de/"><strong>Echtes Netz</strong></a> - Die Kampagne der Digitalen Gesellschaft f&uuml;r Netzneutralit&auml;t in Deutschland.</li>
 	<li><a href="http://pro-netzneutralitaet.de/"><strong>Initiative Pro-Netzneutralit&auml;t</strong></a> - Eine weitere Initiative f&uuml;r Netzneutralit&auml;t.</li>
+	<li><a href="http://www.vzbv.de/mediapics/netzneutralitaet_tacd_resolution_04_2010.pdf"><strong>Resolution zur Netzneutralit&auml;t des Trans Atlantischen Konsumer Dialoges</strong></a></li>
 	<li><a href="http://de.wikipedia.org/wiki/Netzneutralit%C3%A4t"><strong>Wikipedia-Artikel</strong></a> zu Netzneutralit&auml;t in deutscher Sprache.</li>
-	
-
 	<li><a href="http://derstandard.at/1297818927530/Telekom-Austria-Wir-besitzen-die-Infrastruktur-Wir-bestimmen-Punkt"><strong>Ametsreiter (CEO von A1) zu Netzneutralit&auml;t</strong></a> - Artikel auf derstandard.at</li>
-	
 	<li><a href="http://futurezone.at/tag/148-netzneutralitaet.php"><strong>futurezone.at</strong></a> - Artikelsammlung</li>
 	<li><a href="http://www.spiegel.de/thema/netzneutralitaet/"><strong>spiegel.de</strong></a> - Artikelsammlung</li>
-
-	
-	
 	
 </ul>
 			
