7e227b371987151a8e59c969f6670454a754c4b6
socialhack <thomas@lohninger.org>
2012-12-07 02:34:59 +0100

    add page for charts of berec report net neutrality violations

---
+++ links/berec-eu-violations/index.php
@@ -0,0 +1,7 @@
+<?php include($_SERVER['DOCUMENT_ROOT'].'/includes/header.php'); ?>
+
+    <iframe src="http://infogr.am/violations-of-net-neutrality-in-the-EU-per-affected-user/" width="534" height="861" scrolling="no" frameborder="0" style="border:none;"></iframe>
+
+    <iframe src="http://infogr.am/net-neutrality-violations-in-the-EU-per-provider/" width="534" height="953" scrolling="no" frameborder="0" style="border:none;"></iframe>
+
+<?php include($_SERVER['DOCUMENT_ROOT'].'/includes/footer.php'); ?>
