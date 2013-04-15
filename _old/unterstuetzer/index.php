db30723115db7d77a7159b56464e82ca8a2cb17d
socialhack <thomas@lohninger.org>
2013-04-16 01:32:02 +0200

    removed useless dev directory

--- _old/unterstuetzer/index.php
+++
@@ -1,9 +0,0 @@
-<?php include('../modules/header.php'); ?>
-<?php include('../modules/nav.php'); ?>
-
-<h2 class="cat">Diese Organisationen unterstützen unsere Position</h2>
-
-
-
-			
-<?php include('../modules/footer.php'); ?>
