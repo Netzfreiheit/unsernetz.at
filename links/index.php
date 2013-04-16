cb2d65de046d7b6511bf0d434a3b89cd2968c85d
Thomas Lohninger <thomas@lohninger.org>
2013-04-17 01:40:56 +0200

    we made an synopsis of the laws on net neutrality in Chile, Netherlands and Slovenia

--- links/index.php
+++ links/index.php
@@ -13,6 +13,7 @@
 <ul class="nolist">
 	
 	<li><a href="/files/Positionspapier.pdf"><strong>Positionspapier: Netzneutralit&auml;t in &Ouml;sterreich</strong></a></li>
+	<li><a href="/files/Synopsis_CL_NL_SL.pdf"><strong>Synopse der Gesetzte zur Netzneutralit&auml;t in Chile, den Niederlanden und Slowenien</strong></a></li>
 	<li><a href="https://digitalegesellschaft.de/wp-content/uploads/2012/12/DG_Handbuch_NN.pdf"><strong>Handbuch Netzneutralit&auml;t</strong> der Digitalen Gesellschaft</a></li>
 	<li><a href="http://www.netzkinder.at/netzneutralitat-uber-die-unteilbarkeit-des-internets/"><strong>Vortrag: zur Netzneutralit&auml;t und dieser Kampagne</strong></a> von Thomas Lohninger auf der #DNP12 Konferenz</li>
 	<li><a href="http://www.fhstp.ac.at/forschung/institute_bereiche/institutfuermedienwirtschaft/aktuelles/news/12/aktuelle-neuerscheinung-netzneutralitaet-und-netzbewirtschaftung"><strong>Buch: "Netzneutralit&auml;t und Netzbewirtschaftung"</strong></a> von Prof. (FH) Mag. Dr. Tassilo Pellegrini u. Prof. (FH) Dr. Jan Krone</li>
