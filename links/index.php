d7aff04a78ac98a699be946a9d1d6c3edeba991e
socialhack <thomas@lohninger.org>
2013-05-15 14:56:58 +0200

    refactored links section

--- links/index.php
+++ links/index.php
@@ -8,35 +8,57 @@
 		</div>
 
 		<div class="inset">
-		<h3>Links und Ressourcen</h3>
-			
-<ul class="nolist">
-	
-	<li><a href="/files/Positionspapier.pdf"><strong>Positionspapier: Netzneutralit&auml;t in &Ouml;sterreich</strong></a></li>
-	<li><a href="/files/PositionspapierRTR.pdf"><strong>Stellungnahme auf das Positionspapier der RTR zur Netzneutralit&auml;t</strong></a></li>
-	<li><a href="/files/Synopsis_CL_NL_SL.pdf"><strong>Synopse der Gesetzte zur Netzneutralit&auml;t in Chile, den Niederlanden und Slowenien</strong></a></li>
-	<li><a href="https://digitalegesellschaft.de/wp-content/uploads/2012/12/DG_Handbuch_NN.pdf"><strong>Handbuch Netzneutralit&auml;t</strong> der Digitalen Gesellschaft</a></li>
-	<li><a href="http://www.netzkinder.at/netzneutralitat-uber-die-unteilbarkeit-des-internets/"><strong>Vortrag: zur Netzneutralit&auml;t und dieser Kampagne</strong></a> von Thomas Lohninger auf der #DNP12 Konferenz</li>
-	<li><a href="http://www.fhstp.ac.at/forschung/institute_bereiche/institutfuermedienwirtschaft/aktuelles/news/12/aktuelle-neuerscheinung-netzneutralitaet-und-netzbewirtschaftung"><strong>Buch: "Netzneutralit&auml;t und Netzbewirtschaftung"</strong></a> von Prof. (FH) Mag. Dr. Tassilo Pellegrini u. Prof. (FH) Dr. Jan Krone</li>
-	<li><a href="https://unsernetz.at/files/Steckbrief_Netzneutralitaet_Pellegrini.pdf"><strong>Steckbrief: Netzneutralit&auml;t</strong></a> von Tassilo Pellegrini</li>
-	<li><a href="/links/eu-lex-map/"><strong>Karte der rechtlichen Situation zu Netzneutralit&auml;t in der EU</strong></a></li>
-	<li><a href="/links/berec-eu-violations/"><strong>Grafik zur Verletzung von Netzneutralit&auml;t in der EU</strong></a> (basierend auf Daten des <a href="https://ec.europa.eu/digital-agenda/sites/digital-agenda/files/Traffic%20Management%20Investigation%20BEREC_2.pdf">BEREC Reports Mai 2012</a>)</li>
-	<li><a href="https://netarchitecture.org/"><strong>Barbara van Schewick. 2010. Internet Architecture and Innovation. MIT Press. </strong></a></li>
-	<li><a href="http://echtesnetz.de/"><strong>Echtes Netz</strong></a> - Die Kampagne der Digitalen Gesellschaft f&uuml;r Netzneutralit&auml;t in Deutschland.</li>
-	<li><a href="http://respectmynet.eu/"><strong>Meldestelle f&uuml;r Verst&ouml;&szlig;e gegen die Netzneutralit&auml;t</strong></a></li>
-	<li><a href="http://pro-netzneutralitaet.de/"><strong>Initiative Pro-Netzneutralit&auml;t</strong></a> - Eine weitere Initiative f&uuml;r Netzneutralit&auml;t.</li>
-	<li><a href="http://www.vzbv.de/mediapics/netzneutralitaet_tacd_resolution_04_2010.pdf"><strong>Resolution zur Netzneutralit&auml;t des Trans Atlantischen Konsumer Dialoges</strong></a></li>
-	<li><a href="http://de.wikipedia.org/wiki/Netzneutralit%C3%A4t"><strong>Wikipedia-Artikel</strong></a> zu Netzneutralit&auml;t in deutscher Sprache.</li>
-	<li><a href="http://derstandard.at/1297818927530/Telekom-Austria-Wir-besitzen-die-Infrastruktur-Wir-bestimmen-Punkt"><strong>Ametsreiter (CEO von A1) zu Netzneutralit&auml;t</strong></a> - Artikel auf derstandard.at</li>
-	<li><a href="http://futurezone.at/tag/148-netzneutralitaet.php"><strong>futurezone.at</strong></a> - Artikelsammlung</li>
-	<li><a href="http://www.spiegel.de/thema/netzneutralitaet/"><strong>spiegel.de</strong></a> - Artikelsammlung</li>
-	<li>Radiosendung: <a href="http://www.fro.at/article.php?id=5354"><strong>Netzneutralit&auml;t: Sind alle Bits gleich?</strong></a> mit Tassilo Pellegrini</li>
 
-	
-	
-</ul>
-			
-			
-			
+	<h3>B&uuml;cher &amp; Brosch&uuml;ren</h3>		
+		<ul class="nolist">
+			<li><a href="https://netarchitecture.org/"><strong>Barbara van Schewick. 2010. Internet Architecture and Innovation. MIT Press. </strong></a></li>
+			<li><a href="http://www.fhstp.ac.at/forschung/institute_bereiche/institutfuermedienwirtschaft/aktuelles/news/12/aktuelle-neuerscheinung-netzneutralitaet-und-netzbewirtschaftung"><strong>Buch: "Netzneutralit&auml;t und Netzbewirtschaftung"</strong></a> von Prof. (FH) Mag. Dr. Tassilo Pellegrini u. Prof. (FH) Dr. Jan Krone</li>
+			<li><a href="http://www.amazon.de/Master-Switch-Information-Empires-Vintage/dp/0307390993"><strong>Tim Wu. 2011. The Master Switch: The Rise and Fall of Information Empires. Vintage. </strong></a></li>
+			<li><a href="https://digitalegesellschaft.de/wp-content/uploads/2012/12/DG_Handbuch_NN.pdf"><strong>Handbuch Netzneutralit&auml;t</strong> der Digitalen Gesellschaft</a></li>
+		</ul>
+				
+
+		<h3>Vortr&auml;ge</h3>
+			<ul class="nolist">
+				<li><a href="http://www.netzkinder.at/netzneutralitat-uber-die-unteilbarkeit-des-internets/"><strong>Vortrag: zur Netzneutralit&auml;t und dieser Kampagne</strong></a> von Thomas Lohninger auf der #DNP12 Konferenz</li>
+				<li></li>
+			</ul>	
+
+		<h3>Policy Paper</h3>
+			<ul class="nolist">
+				<li><a href="/files/Positionspapier.pdf"><strong>Positionspapier: Netzneutralit&auml;t in &Ouml;sterreich</strong></a></li>
+				<li><a href="http://rtr.at/nn"><strong>RTR-Positionspapier: Netzneutralit&auml;t</strong></a></li>
+				<li><a href="/files/PositionspapierRTR.pdf"><strong>Stellungnahme auf das Positionspapier der RTR zur Netzneutralit&auml;t</strong></a></li>
+				<li><a href="/files/Synopsis_CL_NL_SL.pdf"><strong>Synopse der Gesetzte zur Netzneutralit&auml;t in Chile, den Niederlanden und Slowenien</strong></a></li>
+				<li><a href="https://unsernetz.at/files/Steckbrief_Netzneutralitaet_Pellegrini.pdf"><strong>Steckbrief: Netzneutralit&auml;t</strong></a> von Tassilo Pellegrini</li>
+				<li><a href="http://www.vzbv.de/mediapics/netzneutralitaet_tacd_resolution_04_2010.pdf"><strong>Resolution zur Netzneutralit&auml;t des Trans Atlantischen Konsumer Dialoges</strong></a></li>
+				<li></li>
+			</ul>	
+
+		<h3>Visualisierungen</h3>
+			<ul class="nolist">
+				<li><a href="/links/eu-lex-map/"><strong>Karte der rechtlichen Situation zu Netzneutralit&auml;t in der EU</strong></a></li>
+				<li><a href="/links/berec-eu-violations/"><strong>Grafik zur Verletzung von Netzneutralit&auml;t in der EU</strong></a> (basierend auf Daten des <a href="https://ec.europa.eu/digital-agenda/sites/digital-agenda/files/Traffic%20Management%20Investigation%20BEREC_2.pdf">BEREC Reports Mai 2012</a>)</li>
+				<li><a href="http://netneutralitymap.org/"><strong>Weltkarte der Netzneutralit&auml;tsverletzungen</strong></a> (basierend auf Glasnost Messdaten von M-Lab)</li>
+			</ul>	
+
+		<h3>befreundete Projekte</h3>
+			<ul class="nolist">
+				<li><a href="http://echtesnetz.de/"><strong>Echtes Netz</strong></a> - Die Kampagne der Digitalen Gesellschaft f&uuml;r Netzneutralit&auml;t in Deutschland.</li>
+				<li><a href="http://respectmynet.eu/"><strong>Meldestelle f&uuml;r Verst&ouml;&szlig;e gegen die Netzneutralit&auml;t</strong></a></li>
+				<li><a href="http://pro-netzneutralitaet.de/"><strong>Initiative Pro-Netzneutralit&auml;t</strong></a> - Eine weitere Initiative f&uuml;r Netzneutralit&auml;t.</li>
+				<li><a href="http://www.measurementlab.net/"><strong>Measurement Lab</strong></a> (Datenpool zur Netzneutralität)</li>
+			</ul>	
+
+		<h3>Medienberichte</h3>
+			<ul class="nolist">
+				<li><a href="http://de.wikipedia.org/wiki/Netzneutralit%C3%A4t"><strong>Wikipedia-Artikel</strong></a> zu Netzneutralit&auml;t in deutscher Sprache.</li>
+				<li><a href="http://derstandard.at/1297818927530/Telekom-Austria-Wir-besitzen-die-Infrastruktur-Wir-bestimmen-Punkt"><strong>Ametsreiter (CEO von A1) zu Netzneutralit&auml;t</strong></a> - Artikel auf derstandard.at</li>
+				<li><a href="http://futurezone.at/tag/148-netzneutralitaet.php"><strong>futurezone.at</strong></a> - Artikelsammlung</li>
+				<li><a href="http://www.spiegel.de/thema/netzneutralitaet/"><strong>spiegel.de</strong></a> - Artikelsammlung</li>
+				<li>Radiosendung: <a href="http://www.fro.at/article.php?id=5354"><strong>Netzneutralit&auml;t: Sind alle Bits gleich?</strong></a> mit Tassilo Pellegrini</li>
+			</ul>	
+
+
 		</div>
 <?php include('../includes/footer.php'); ?>
