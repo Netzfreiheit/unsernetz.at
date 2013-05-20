109efe3188aa7acb01d2a6f13e31c9d1dbb8c186
socialhack <thomas@lohninger.org>
2013-05-20 19:17:32 +0200

    wir waren in mehreren medien mit unserer kampagne

--- links/index.php
+++ links/index.php
@@ -61,6 +61,10 @@
 		<h3>Medienberichte</h3>
 			<ul class="nolist">
 				<!--<li><a href=""><strong></strong></a></li>-->
+				<!--<li><a href=""><strong></strong></a> in Futurezone (21. Mai 2013)</li>-->
+				<li>Radio: <a href="http://oe1.orf.at/programm/337591"><strong>Zwei-Klassen-Internet auch in Österreich möglich </strong></a> in Ö1 Help (18. Mai 2013) <a href="http://socialhack.eu/uploads/media-archive/OE1_HEL130518_HP.mp3">mp3 hier</a> </li>
+				<li>Radio: <a href="http://oe1.orf.at/programm/337583"><strong>Zwei-Klassen-Internet auch in Österreich möglich </strong></a> in Ö1 Morgenjournal (18. Mai 2013) <a href="http://socialhack.eu/uploads/media-archive/oe1_journal_drosselkom_2013-05-18.mp3">mp3 hier</a> </li>
+				<li>Online: <a href="http://help.orf.at/stories/1717952/"><strong>Zwei-Klassen-Internet auch in Österreich möglich</strong></a> in Help auf orf.at (18. Mai 2013)</li>
 				<li><a href="http://oe1.orf.at/programm/337279"><strong>Einordnung des RTR Positionspapiers </strong></a> &Ouml;1 Digital Leben (15. Mai 2013)</li>
 				<li><a href="http://derstandard.at/1363709940095/Fall-Drosselkom-Provider-wollen-Netzneutralitaet-abschaffen"><strong>Fall Drosselkom: "Provider wollen Netzneutralit&auml;t abschaffen"</strong></a> Gastkommentar auf derStandard.at (3. Mai 2013)</li>
 				<li><a href="https://netzpolitik.org/2013/vergleich-verschiedener-netzneutralitatsgesetze/"><strong>Vergleich verschiedener Netzneutralit&auml;tsgesetze</strong></a> Artikel auf Netzpolitik.org (26. April 2013)</li>
