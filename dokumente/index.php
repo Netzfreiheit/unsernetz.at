09b5b3c371f07a64084fcad058b3968919a77f0d
socialhack <thomas@lohninger.org>
2013-05-27 00:51:15 +0200

    add link to local pdf

--- dokumente/index.php
+++ dokumente/index.php
@@ -11,17 +11,17 @@
     <div class="inset">
 
         <p style="font-weight:bold;">
-           <a title="View Positionspapier: Netzneutralit&auml;t in &Ouml;sterreich on Scribd" href="http://www.scribd.com/doc/141603290/Positionspapier-Netzneutralitat-in-Osterreich"  style="text-decoration: underline;" >Positionspapier: Netzneutralit&auml;t in &Ouml;sterreich</a>
+           <a title="View Positionspapier: Netzneutralit&auml;t in &Ouml;sterreich on Scribd" href="http://www.scribd.com/doc/141603290/Positionspapier-Netzneutralitat-in-Osterreich"  style="text-decoration: underline;" >Positionspapier: Netzneutralit&auml;t in &Ouml;sterreich</a> (<a href="/files/Positionspapier.pdf">PDF</a>)
         </p>
         <iframe class="scribd_iframe_embed" src="http://www.scribd.com/embeds/141603290/content?start_page=1&view_mode=scroll" data-auto-height="false" data-aspect-ratio="undefined" scrolling="no" id="doc_21291" width="100%" height="600" frameborder="0"></iframe>
 
         <p style="font-weight:bold;">
-            <a title="View Stellungnahme auf RTR-Positionspapier zur Netzneutralit&auml;t on Scribd" href="http://www.scribd.com/doc/141381392/Stellungnahme-auf-RTR-Positionspapier-zur-Netzneutralitat"  style="text-decoration: underline;" >Stellungnahme auf RTR-Positionspapier zur Netzneutralit&auml;t</a>
+            <a title="View Stellungnahme auf RTR-Positionspapier zur Netzneutralit&auml;t on Scribd" href="http://www.scribd.com/doc/141381392/Stellungnahme-auf-RTR-Positionspapier-zur-Netzneutralitat"  style="text-decoration: underline;" >Stellungnahme auf RTR-Positionspapier zur Netzneutralit&auml;t</a> (<a href="/files/PositionspapierRTR.pdf">PDF</a>)
         </p>
         <iframe class="scribd_iframe_embed" src="http://www.scribd.com/embeds/141381392/content?start_page=1&view_mode=scroll&access_key=key-1ngcfnwj7s4olk9qj20w" data-auto-height="false" data-aspect-ratio="0.772922022279349" scrolling="no" id="doc_1030" width="100%" height="600" frameborder="0"></iframe>
 
         <p style="font-weight:bold;">
-            <a title="View Synopsis Netzneutralit&auml;tsgesetze on Scribd" href="http://www.scribd.com/doc/138101920/Synopsis-Netzneutralitatsgesetze"  style="text-decoration: underline;" >Synopsis Netzneutralit&auml;tsgesetze</a>
+            <a title="View Synopsis Netzneutralit&auml;tsgesetze on Scribd" href="http://www.scribd.com/doc/138101920/Synopsis-Netzneutralitatsgesetze"  style="text-decoration: underline;" >Synopsis Netzneutralit&auml;tsgesetze</a> (<a href="/files/Synopsis_CL_NL_SL.pdf">PDF</a>)
         </p>
         <iframe class="scribd_iframe_embed" src="http://www.scribd.com/embeds/138101920/content?start_page=1&view_mode=scroll&access_key=key-abk75737xuc28h3u6t0" data-auto-height="false" data-aspect-ratio="1.4160125588697" scrolling="no" id="doc_61369" width="100%" height="600" frameborder="0"></iframe>
 
