542251f97d7577ade07a38a0f6b9ed5e330833bb
Thomas Lohninger <thomas@lohninger.org>
2013-06-10 03:03:19 +0200

    new arguments

--- argumente/index.php
+++ argumente/index.php
@@ -35,6 +35,33 @@
 <h4>Netzneutralit&auml;t ist ein Standortvorteil</h4>
 <p> Durch  die Bedeutung der Netzneutralit&auml;t f&uuml;r technische Innovationen w&uuml;rde  eine Sicherung der Netzneutralit&auml;t im Telekommunikationsgesetz einen  wirtschaftlichen Standortvorteil bieten. F&uuml;r aufstrebende, innovative  Unternehmen im IKT-Umfeld w&uuml;rde &Ouml;sterreich dadurch attraktiver, insbesondere, da in anderen EU-Staaten der Trend in Richtung zunehmender Diskriminierung des Datenverkehrs deutlich wird. </p>
 </li>
+<li>
+	<h4>Was bedeutet "doppelter Markt" und was hat das mit Netzneutralit&auml;t zu tun?</h4>
+	<p>
+In der <a href="http://rtr.at/nn">RTR Position</a> wird die M&ouml;glichkeit f&uuml;r nicht netzneutrale Produktgestaltung er&ouml;ffnet (Prinzip 5). Dadurch wird den ISPs die M&ouml;glichkeit geschaffen ihre Internet-Zugangsprodukte mit Angeboten von Dienste- und Inhaltsanbietern zu koppeln. Diese vertikale Integration von Internet-Zugang mit Diensten, die &uuml;ber das Internet genutzt werden, ist der Kern der Netzneutralit&auml;ts-Debatte geworden. <br/>
+<br/>
+Durch diese Koppelung eines Internet Anschlusses haben vor allem gro&szlig;e, internationale Anbieter die M&ouml;glichkeit sich bevorzugten Zugang zu ihren potentiellen Kunden zu kaufen und dadurch die eigene Marktposition zu zementieren. Die Gegenleistung der ISPs besteht darin diese Dienste von Volumen Tarifen auszunehmen oder ihnen beschleunigte Durchleitung durch die eigenen Netze zu gew&auml;hren. <br/>
+<br/>
+Wir sehen solche Tarife und Wettbewerbsnachteile bereits in Deutschland durch den Spotify-Tarif der Deutschen Telekom. Wenn ein solches zwei-Klassen Internet nach den Vorstellungen von Neelie Kroes und RTR erlaubt wird, f&uuml;hrt dies einerseits dazu dass nicht-kommerzielle Angebote (Wikipedia), Start-Ups und vor allem neue innovative Entwicklungen sich nicht mehr gleichberechtigt des neutralen Internets bedienen k&ouml;nnen. Damit w&auml;ren <a href="https://netarchitecture.org/">Erfolgsrezepte des Internets</a> wie das "Ende-zu-Ende" Prinzip oder "Innovation without permission" Geschichte. <br/>
+<br/>
+Man bezeichnet diesen Punkt auch doppelten Markt, weil dadurch die Inhalte- und Diensteanbieter nicht mehr nur gegen&uuml;ber dem Kunden, sonder auch gegen&uuml;ber dem ISP konkurrieren um bevorzugten Zugang zu dessen Kunden zu kaufen. Vor allem gro&szlig;e Internet Provider wie A1 und UPC sind hier in einer Machtposition und k&ouml;nnen ihre Kunden als Faustpfand in Verhandlungen verwenden. Dies f&uuml;hrt nicht zu mehr Wettbewerb und eingeschr&auml;nkter Wahlfreiheit der Konsumenten. 
+	</p>
+	</li>
+	<li>
+		<h4>Was haben die Drossel Pl&auml;ne der Deutschen Telekom mit Netzneutralit&auml;t zu tun? Stichwort: Drosselkom</h4>
+		<p>
+			Durch diesen Doppelten Markt verringert sich die wirtschaftliche Notwendigkeit f&uuml;r ISPs in ihre Netze zu investieren. Das eigentliche Produkt der ISPs ist dann nicht mehr nur der Internet-Zugang, sondern auch Zusatzpakete f&uuml;r einzelne Dienste- oder Inhaltsangebote. Diese Zusatzpakete erlauben ISPs komplett neue Produktdifferenzierungen und Erl&ouml;squellen.<br/>
+			Je reduzierter der neutrale Anteil eines Internet-Anschlusses ist, umso gr&ouml;&szlig;er ist der Anreiz f&uuml;r Konsumenten und Inhalte- und Diensteanbieter eine Bevorzugung im Netzwerk zu kaufen. Der ISP kann dadurch mehr Produkte an seine Kunden verkaufen ohne noch in die eigenen Netze investieren zu m&uuml;ssen. Vor diesem Hintergrund ist auch die Einf&uuml;hrung einer Drosselung von Festnetz-Tarifen zu verstehen, wie sie die Deutsche Telekom k&uuml;rzlich <a href="http://www.telekom.com/medien/produkte-fuer-privatkunden/184370">angek&uuml;ndigt hat</a>. In den Niederlanden, wo es bereits ein Gesetz zur Sicherung der Netzneutralit&auml;t gibt, sind <a href="https://netzpolitik.org/2013/fakten-check-hat-netzneutralitat-in-den-niederlanden-internet-langsamer-und-teurer-gemacht/#more-49492">die Preise f&uuml;r Internet &uuml;brigens gesunken</a> und die angebotenen Bandbreiten &amp; Datenvolumen gestiegen. 
+		</p>
+	</li>
+	<li>
+		<h4>Die EU-Kommission sagt "Transparenz &amp; Wettbewerb" reichen um Netzneutralit&auml;t zu l&ouml;sen. Ist das so?</h4>
+		<p>Wettbewerb und Transparenz werden oft als L&ouml;sung f&uuml;r Netzneutralit&auml;t dargestellt. Wenn es einen doppelten Markt gibt, ist die Wahlfreiheit des Konsumenten jedoch prinzipiell schon eingeschr&auml;nkt, weil Sie sich bei der Wahl des ISPs auch f&uuml;r gewisse Dienste- und Inhaltsanbieter entscheiden m&uuml;ssen und die ISPs mit ihrer Produktgestaltung schon Entscheidungen f&uuml;r die Kunden treffen. Die Erwartungshaltung aller Internet Nutzer ist es immer ein uneingeschr&auml;nktes Internet zu kaufen und damit auch alles tun zu k&ouml;nnen, eine Abkehr davon kann nicht im Interesse der Konsumenten sein. </p>
+	</li>
+	<li>
+		<h4>Wo braucht man Transparenz um Netzneutralt&auml;t sicherzustellen?</h4>
+		<p>Transparenz ist definitiv notwendig f&uuml;r eine L&ouml;sung der Netzneutralt&auml;ts-Frage, aber alleine nicht ausreichend um das Problem zu l&ouml;sen. Es gibt zwei Arten von Netzneutralit&auml;tsverletzungen, welche transparent gemacht werden sollten: einerseits in der Produktgestaltung von ISPs, wo Transparenz oft als Feigenblatt verwendet und an den dahinter liegenden Problemen nichts &auml;ndern und zweitens in der Frage des Allt&auml;glichen Netzwerkmangments wenn Datenstaus auftreten oder die Netzwerkstabilit&auml;t hergestellt werden muss. Im zweiten Punkt ist Transparenz eine gute L&ouml;sung.</p>
+	</li>
 </ul>
 
 			
