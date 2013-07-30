db8852e1c66e3cadea5808f362816283417fcf89
Thomas Lohninger <thomas@lohninger.org>
2013-07-30 02:58:05 +0200

    moved piwik code from footer to header

--- includes/footer.php
+++ includes/footer.php
@@ -1,28 +1,9 @@
 	</div>
-	
 	<div id="footer-wrapper">
 		<div class="inset">
 			<p>unsernetz.at - Ein Projekt der <a href="https://netzfreiheit.org" target="_blank">Initiative f&uuml;r Netzfreiheit</a>, <a href="https://vibe.at" target="_blank">VIBE!AT</a> und <a href="http://gooutsidewien.wordpress.com/" target="_blank">Go Outside</a></p>
 		</div>
 	</div>
-	
 </div>
-
-
 </body>
-
 </html>
-
-
-<!-- Piwik --> 
- <script type="text/javascript">
- var pkBaseURL = "https://piwik.netzfreiheit.org/";
- document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
- </script><script type="text/javascript">
- try {
- var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 4);
- piwikTracker.trackPageView();
- piwikTracker.enableLinkTracking();
- } catch( err ) {}
- </script><noscript><p><img src="https://piwik.netzfreiheit.org/piwik.php?idsite=4" style="border:0" alt="" /></p></noscript>
- <!-- End Piwik Tracking Code -->	
--- includes/header.php
+++ includes/header.php
@@ -7,6 +7,20 @@
 	<link rel="stylesheet" href="/css/style.css" type="text/css" media="screen" />
 	<script type="text/javascript" src="/js/jquery-1.8.2.min.js"></script>
 	<script type="text/javascript" src="/js/teasers.js"></script>
+
+		<!-- Piwik --> 
+	 <script type="text/javascript">
+	 var pkBaseURL = "https://piwik.netzfreiheit.org/";
+	 document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
+	 </script><script type="text/javascript">
+	 try {
+	 var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 4);
+	 piwikTracker.trackPageView();
+	 piwikTracker.enableLinkTracking();
+	 } catch( err ) {}
+	 </script><noscript><p><img src="https://piwik.netzfreiheit.org/piwik.php?idsite=4" style="border:0" alt="" /></p></noscript>
+	 <!-- End Piwik Tracking Code -->	
+
 </head>
 
 <body>
